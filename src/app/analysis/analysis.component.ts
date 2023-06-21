ea586273ad0f5f6587e09f71332bb4b6d2201bf2
Ranjitha.l <Ranjitha.l@JKTBLRLAP521>
2023-06-21 18:59:56 +0530

    analysis screen added

--- src/app/analysis/analysis.component.ts
+++ src/app/analysis/analysis.component.ts
@@ -11,5 +11,27 @@ export class AnalysisComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  flamboyant: number[] = [15, 25, 35, 45, 55,15, 25, 35, 45, 55, 25, 20]; 
+  purist: number[] = [15, 25, 35, 45, 55,15, 25, 35, 45, 55, 25, 22];
+  sensible: number[] = [15, 25, 35, 45, 55,15, 25, 35, 45, 55, 25, 26]; 
+  aspirant: number[] = [15, 25, 35, 45, 55,15, 25, 35, 45, 55, 25, 32];
+  hoverer: number[] = [15, 25, 35, 45, 55,15, 25, 35, 45, 55, 25, 52];
+  neutralising: number[] =[15]
+ 
+
+  getBackgroundColor(value: number): string {
+    if (value >= 10 && value <= 20) {
+      return "#89CFF0";
+    } else if (value > 20 && value <= 30) {
+      return "#6CB4EE";
+    } else if (value > 30 && value <= 40) {
+      return "#5072A7";
+    } else if (value > 40 && value <= 50) {
+      return "#318CE7";
+    } else if (value > 50 && value <= 60) {
+      return "#0039a6";
+    }
+    return ""; // Return empty string for no background color
+  }
 
 }
